1f402d46d7d9d322d8d8f8faa607c721db7ca4ea
danny01-ops <aidanclaude.tsang@benilde.edu.ph>
2022-04-02 18:39:02 +0800

    Fixed homepage after login

--- src/main/java/com/appadvc/finalproj/security/SecurityConfiguration.java
+++ src/main/java/com/appadvc/finalproj/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "/login*", "/register*", "/forgot-password*",
                         "/css/**", "/js/**", "/img/**")
-                .anonymous()
+                .permitAll()
                 .antMatchers("/user*").hasRole("ADMIN")
                 //.antMatchers("/product*").hasAnyRole("ADMIN", "EMPLOYEE")
                 .anyRequest()
